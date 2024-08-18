40edbd362ac74fc4296f595bce29143184ac994d
ChiaYEHHH <hedy821216@gmail.com>
2024-08-19 00:47:59 +0800

    完成老劉影片6/14進度

---
+++ 0816/api/edit.php
@@ -0,0 +1,48 @@
+<?php
+include_once "base.php";
+dd($_POST);
+$do=$_POST['table'];
+$db=${ucfirst($do)};
+
+foreach($_POST['id'] as $key => $id){
+    if(!empty($_POST['del']) && in_array($id,$_POST['del'])){
+        $db->del($id);  //in_array($id,$_POST['del'])確保$id有在$_POST['del']陣列中
+    }else{
+        $data = $db->find($id); //先把要修改的整筆資料撈出來存在$data
+        dd($data);
+        switch($do){
+            case 'title':
+                $data['sh']=(isset($_POST['sh']) && $_POST['sh'] == $id)? 1:0;     
+                $data['text']=$_POST['text'][$key];
+                break;
+            case 'ad':
+                $data['sh']=(isset($_POST['sh']) && in_array($id,$_POST['sh'])) ? 1:0;     
+                $data['text']=$_POST['text'][$key];
+                break;
+            case 'mvim':
+            case 'image':
+                $data['sh']=(isset($_POST['sh']) && in_array($id,$_POST['sh'])) ? 1:0;
+                break;
+            case 'total':
+
+                break;
+            case 'bottom':
+
+                break;
+            case 'news':
+
+                break;
+            case 'admin':
+
+                break;
+            case 'menu':
+
+                break;
+        }
+
+        dd($data);
+        $db->save($data);
+    }
+}
+
+to("../admin.php?do=$do");
---
+++ 0816/api/update.php
@@ -0,0 +1,20 @@
+<?php
+include_once "base.php";
+dd($_POST);
+$do = $_POST['table'];
+$db = ${ucfirst($do)};
+
+
+if(!empty($_FILES['img']['tmp_name'])){
+    move_uploaded_file($_FILES['img']['tmp_name'],"../images/" . $_FILES['img']['name']);
+    $data = $db->find($_POST['id']); //先把要修改的整筆資料撈出來存在$data
+    $data['img']=$_FILES['img']['name'];
+    dd($data);
+
+    $db->save($data);
+}
+
+
+
+
+to("../admin.php?do=$do");
